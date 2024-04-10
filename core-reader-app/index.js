641f552c65502ce48dd2c6091f2a0afc4a4c5ca0
alemanjosh7 <josuealeman2003@gmail.com>
2024-04-10 12:32:26 -0600

    Add changes to blocks and core reader

--- core-reader-app/index.js
+++ core-reader-app/index.js
@@ -1,32 +1,27 @@
 import Hyperswarm from 'hyperswarm'
 import Corestore from 'corestore'
 import b4a from 'b4a'
-
+import process from 'bare-process'
 import { Node } from 'hyperbee/lib/messages.js'
-
+console.log(process.argv[2].substring(2, process.argv[2].length))
 const store = new Corestore('./reader-storage')
 
 const swarm = new Hyperswarm()
 Pear.teardown(() => swarm.destroy())
 
 swarm.on('connection', conn => store.replicate(conn))
-console.log(process.argv[2])
-// // create or get the hypercore using the public key supplied as command-line argument
-// const core = store.get({ key: b4a.from(process.argv[2], 'hex') })
-// // wait till the properties of the hypercore instance are initialized
-// await core.ready()
 
-// const foundPeers = store.findingPeers()
-// // join a topic
-// swarm.join(core.discoveryKey)
-// swarm.flush().then(() => foundPeers())
+const core = store.get({ key: b4a.from(process.argv[2].substring(2, process.argv[2].length), 'hex') })
+await core.ready()
 
-// // update the meta-data information of the hypercore instance
-// await core.update()
+const foundPeers = store.findingPeers()
+swarm.join(core.discoveryKey)
+swarm.flush().then(() => foundPeers())
 
-// const seq = core.length - 1
-// const lastBlock = await core.get(core.length - 1)
+await core.update()
 
-// // print the information about the last block or the latest block of the hypercore instance
-// console.log(`Raw Block ${seq}:`, lastBlock)
-// console.log(`Decoded Block ${seq}`, Node.decode(lastBlock))
+const seq = core.length - 1
+const lastBlock = await core.get(core.length - 1)
+
+console.log(`Raw Block ${seq}:`, lastBlock)
+console.log(`Decoded Block ${seq}`, Node.decode(lastBlock))
